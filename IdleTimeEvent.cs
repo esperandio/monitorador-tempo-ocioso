ceb4ba127f52aa92062fdb2ef337719ed23903d5
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2021-09-12 01:33:41 -0300

    feat: criacao de estrutura de dados referente a um evento de tempo ocioso

---
+++ IdleTimeEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MonitoradorTempoOcioso
+{
+    struct IdleTimeEvent
+    {
+        public DateTime DateTime;
+        public uint SecondsIdle;
+
+        public IdleTimeEvent(DateTime dateTime, uint secondsIdle)
+        {
+            DateTime = dateTime;
+            SecondsIdle = secondsIdle;
+        }
+    }
+}
