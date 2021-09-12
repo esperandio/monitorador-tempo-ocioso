1bd3f1c3a5a7f84b2187a092f3c388c52ed76e98
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2021-09-12 01:37:56 -0300

    feat: criacao de classe para monitorar o tempo ocioso do usuario e persistir o log em repositorio

---
+++ IdleTimeObserver.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace MonitoradorTempoOcioso
+{
+    class IdleTimeObserver
+    {
+        private Timer _timer;
+        private readonly IEventRepository _eventRepository;
+        private uint _totalIdleTime = 0;
+        private readonly uint _maxIdleTimeAllowed = 5000;
+
+        public IdleTimeObserver(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+
+        public void Start()
+        {
+            _timer = new Timer(TimerCallback, null, 0, 1000);
+        }
+
+        private void TimerCallback(Object o)
+        {
+            uint idleTime = IdleTimeFinder.GetIdleTime();
+
+            if (idleTime < _totalIdleTime)
+            {
+                Notify(idleTime);
+                _totalIdleTime = 0;
+                return;
+            }
+
+            if (idleTime < _maxIdleTimeAllowed)
+            {
+                return;
+            }
+
+            _totalIdleTime = idleTime;
+        }
+
+        public void Stop()
+        {
+            _timer.Dispose();
+        }
+
+        private void Notify(uint idleTime)
+        {
+            _eventRepository.Add(new IdleTimeEvent(DateTime.Now, idleTime));
+        }
+    }
+}
