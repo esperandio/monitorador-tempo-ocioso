07b887d9aac4d9501d8117219f9f88b35c9f5ae8
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2021-09-18 03:22:12 -0300

    feat: criacao da struct StopEvent para identificar o evento de pausar monitoramento

---
+++ StopEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MonitoramentoTempoOcioso
+{
+    struct StopEvent : IEvent
+    {
+        public DateTime DateTime;
+
+        public StopEvent(DateTime dateTime)
+        {
+            DateTime = dateTime;
+        }
+
+        public string SerializeObject()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(new {
+                ds_event = "StopEvent",
+                dt_event = DateTime.ToString("yyyy-MM-dd HH:mm:ss")
+            });
+        }
+    }
+}
