5ab0e803466a21f4026c3e0d1561d0a3fe3ab3f1
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2021-09-18 03:21:41 -0300

    feat: criacao da struct StartEvent para identificar o evento de inicializacao do monitoramento

---
+++ StartEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MonitoramentoTempoOcioso
+{
+    struct StartEvent : IEvent
+    {
+        public DateTime DateTime;
+
+        public StartEvent(DateTime dateTime)
+        {
+            DateTime = dateTime;
+        }
+
+        public string SerializeObject()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(new {
+                ds_event = "StartEvent",
+                dt_event = DateTime.ToString("yyyy-MM-dd HH:mm:ss")
+            });
+        }
+    }
+}
