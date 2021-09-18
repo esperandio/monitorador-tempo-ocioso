fcfe492227ce8a0985ca9237790e766bdc6440f9
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2021-09-18 13:49:05 -0300

    feat: criacao da struct StartApplicationEvent para identificar o evento de inicializacao da aplicacão

---
+++ StartApplicationEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MonitoramentoTempoOcioso
+{
+    struct StartApplicationEvent : IEvent
+    {
+        public DateTime DateTime;
+
+        public StartApplicationEvent(DateTime dateTime)
+        {
+            DateTime = dateTime;
+        }
+
+        public string SerializeObject()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(new
+            {
+                ds_event = "StartApplicationEvent",
+                dt_event = DateTime.ToString("yyyy-MM-dd HH:mm:ss")
+            });
+        }
+    }
+}
