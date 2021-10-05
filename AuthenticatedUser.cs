07ff0f29eca18b0f1a75c7dbf5293f665c64bd6b
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2021-10-04 22:34:50 -0300

    refactor: renomeacao de variavel

--- AuthenticatedUser.cs
+++ AuthenticatedUser.cs
@@ -6,7 +6,7 @@ namespace MonitoramentoTempoOcioso
 {
     class AuthenticatedUser : IUser
     {
-        static readonly HttpClient client = new HttpClient();
+        private static readonly HttpClient s_client = new HttpClient();
         private readonly string _userName;
 
         private AuthenticatedUser(string userName)
@@ -16,8 +16,8 @@ namespace MonitoramentoTempoOcioso
 
         public static async Task<AuthenticatedUser> AuthenticateAsync(string userName, string password)
         {
-            var response = await client.PostAsync(
-                "http://api.webhookinbox.com/i/x0GhMH52/in/",
+            var response = await s_client.PostAsync(
+                "http://api.webhookinbox.com/i/x0GhMH52/ina/",
                 new StringContent(
                     Newtonsoft.Json.JsonConvert.SerializeObject(
                         new {
